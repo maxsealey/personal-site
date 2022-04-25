ea01ec88fae26967775a567456abb66b0ef95d6f
Max Sealey <maxsealey@users.noreply.github.com>
2022-04-25 10:47:22 -0700

    created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style.css';
+
+export class Footer extends React.Component {
+    render() {
+        return (
+            <div id = "footer">
+                <p className = "text-center">Copyright &copy; 2022 Max Sealey</p>
+            </div>
+        )
+    }
+}
