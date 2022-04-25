f1c55f98e4de136aace48786d20ac20d0d38656c
Max Sealey <maxsealey@users.noreply.github.com>
2022-04-25 10:47:48 -0700

    created Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style.css';
+
+export class Nav extends React.Component {
+    render (){
+        return (
+            <div className="nav-bar"></div>
+        )
+    }
+}
