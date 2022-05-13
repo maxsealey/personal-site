4a9e10d6d79ed4f8a0232c514394f156fe15963b
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-13 14:41:27 -0700

    Implemented Navbar and style objects

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -3,8 +3,18 @@ import '../../style.css';
 
 export default class Nav extends React.Component {
     render (){
+        const navStyle = { height:'70px', display:'flex', justifyContent:'space-between', position:'absolute' }
+        const linkStyle = { color:'white', textDecoration:'none', cursor:'pointer' }
+
         return (
-            <div className="nav-bar"></div>
+            <div>
+            <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top' 
+            style={ navStyle }>
+                <a href="#" style={ linkStyle }>
+                    <h3 style={{margin:'auto', marginLeft:'20px'}}>Max Sealey</h3>
+                </a>
+            </nav>
+        </div>
         )
     }
 }
