c0e5ddf7dda1fea9592acae96c1f4929909a0964
Max Sealey <maxsealey@users.noreply.github.com>
2022-06-02 17:30:57 -0700

    populated education data array of objects

---
+++ src/data/Education.js
@@ -0,0 +1,34 @@
+export const education = [
+    {
+        "Degree":"B.S. in Computer Science",
+        "School":"Western Governors University",
+        "Graduation Date":"Expected August 2023",
+        "Courses":
+            {
+                "":"",
+            }
+    },
+    {
+        "Degree":"B.S. in Political Science",
+        "School":"Arizona State University",
+        "Honors":"Magna cum laude",
+        "Graduation Date":"May 2021",
+        "Courses":
+            {
+                "CIS 300":"Web Design & Development",
+                "POS 401":"Political Statistics"
+            }
+    },
+    {
+        "Degree":"No Degree - Computer Science major",
+        "School":"Washington State University",
+        "Courses":
+            {
+                "Cpt_S 111":"Algorithmic Problem Solving",
+                "Cpt_S 121":"Program Design & Development",
+                "Cpt_S 122":"Data Structures",
+                "Math 216":"Discrete Structures",
+                "Math 171":"Calculus I"
+            }
+    }
+] // education data to export
---
+++ src/data/Projects.js
@@ -0,0 +1,7 @@
+export const projects = [
+    {
+        "Title":"",
+        "Tech":"",
+        "Description":"",
+    }
+]
