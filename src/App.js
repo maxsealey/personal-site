03738316473c2285bcb8edba70ceb9e687adfd55
Max Sealey <maxsealey@users.noreply.github.com>
2022-06-17 22:35:55 -0700

    import and deploy Routes, minor index.scss adjustments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import './app.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Home from './components/home/Home'
-import Portfolio from './components/portfolio/Portfolio'
+import Projects from './components/projects/Projects'
 import Experience from './components/experience/Experience';
 import Contact from './components/contact/Contact'
 import Layout from './components/layout/Layout';
@@ -14,7 +14,9 @@ class App extends React.Component {
     return (
       <>
         <div className="app">
-          <Layout/>
+          <Routes>
+            <Route path="/" element={<Layout/>}/>
+          </Routes>
         </div>
       </>
     )
