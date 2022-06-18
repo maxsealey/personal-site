d4f08b803833a9372b25963a8cef62b9575103d6
Max Sealey <maxsealey@users.noreply.github.com>
2022-06-18 14:43:06 -0700

    Working on Home page component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import { Outlet } from 'react-router-dom'
 import Nav from '../nav/Nav'
 import './layout.scss'
 
 export default class Layout extends Component {
   render() {
     return (
-      <div>
+      <div className='app'>
         <Nav/>
+        <div className='page'>
+          <Outlet/>
+        </div>
       </div>
     )
   }
