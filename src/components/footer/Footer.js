38bfd7d9338ce495cb78d4d459f71f6ee3dc0403
Max Sealey <maxsealey@users.noreply.github.com>
2022-06-17 22:39:30 -0700

    reconfigured directories, renamed portfolio to projects, modularized layout styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './layout.scss'
+import './footer.scss'
 
 export default class Footer extends Component {
     render() {
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './projects.scss'
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div className='projects'>
+        
+      </div>
+    )
+  }
+}
