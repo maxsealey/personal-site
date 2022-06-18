1eec03d323d520277d548980d4ab1318a0fe3e66
Max Sealey <maxsealey@users.noreply.github.com>
2022-06-18 01:11:02 -0700

    created Skills component, added to Nav, added profile pic to Nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faCode, faBriefcase, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faCode, faBriefcase, faEnvelope, faLaptop } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import Profile from '../../assets/profile.jpeg'
 import './nav.scss'
 
 export default class Nav extends Component {
     render(){
         return (
             <div className='nav'>
+                <Link className='profile' to="/">
+                    <img src={Profile} alt="my goofy ass face"></img>
+                </Link>
                 <nav>
                     <NavLink exact="true" activeclassname="active" className="home-link" to="/home">
                         <FontAwesomeIcon icon={faHome} color="#f7fff7"/>
@@ -17,10 +21,13 @@ export default class Nav extends Component {
                         <FontAwesomeIcon icon={faUser} color="#f7fff7"/>
                     </NavLink>
                     <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
-                        <FontAwesomeIcon icon={faCode} color="#f7fff7"/>
+                        <FontAwesomeIcon icon={faLaptop} color="#f7fff7"/>
                     </NavLink>
                     <NavLink exact="true" activeclassname="active" className="experience-link" to="/experience">
                         <FontAwesomeIcon icon={faBriefcase} color="#f7fff7"/>
+                    </NavLink>
+                    <NavLink exact="true" activeclassname="active" className="skills-link" to="/skills">
+                        <FontAwesomeIcon icon={faCode} color="#f7fff7"/>
                     </NavLink>
                     <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                         <FontAwesomeIcon icon={faEnvelope} color="#f7fff7"/>
---
+++ src/components/skills/Skills.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './skills.scss'
+
+export default class Skills extends Component {
+  render() {
+    return (
+      <div className='skills'></div>
+    )
+  }
+}
