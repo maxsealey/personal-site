7d22853aa1411f594f604715a7ef6ac231d17e02
Max Sealey <maxsealey@users.noreply.github.com>
2022-06-23 10:50:03 -0700

    converted components to arrow functions, reorganized directories

---
+++ src/components/pages/contact/Contact.js
@@ -0,0 +1,7 @@
+import './contact.scss'
+
+const Contact = () => {
+    return (
+      <div className="contact"></div>
+    )
+}
---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className="home home-container">
+      <div className="summary">
+        <h1>Hey, my name is Max!</h1><br/>
+        <h3>Programmer | Web Developer | Problem Solver</h3><br/>
+        <h4>Currently studying Computer Science at Western Governors University.</h4><br/>
+        <h4>Java | JavaScript | Python</h4>
+      </div>
+    </div>
+  )
+}
