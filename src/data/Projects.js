c117c5a26c0a36cf7820c4e8f5197e51c8cca663
Max Sealey <maxsealey@users.noreply.github.com>
2023-04-08 13:39:11 -0700

    Added some project data

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,15 +1,24 @@
 export const projects = [
     {
         "id":1,
-        "Title":"",
-        "Languages":[],
-        "Frameworks":[],
-        "GitLink":"",
-        "DemoLink":"",
-        "Description":""
+        "Title":"Personal Portfolio Site",
+        "Languages":["JavaScript"],
+        "Frameworks":["React","Redux","Node","Express"],
+        "GitLink":"https://https://github.com/maxsealey/personal-site",
+        "DemoLink":"https://maxsealey.com",
+        "Description":"Personal website created from scratch using a React/Redux front-end and Node/Express backend."
     },
     {
         "id":2,
+        "Title":"JavaFX Inventory System",
+        "Languages":["Java","FXML"],
+        "Frameworks":["JavaFX"],
+        "GitLink":"https://https://github.com/maxsealey/JavaFX-InventorySystem",
+        "DemoLink":"N/A",
+        "Description":"CRUD Application implemented with Java, using the JavaFX framework and Scene Builder to create the Graphical User Interface. Allows the user to add, modify, and remove, parts and products from a fictional Inventory system. Originally created for Software I Final Project, though enhancements have been made."
+    },
+    {
+        "id":3,
         "Title":"",
         "Languages":[],
         "Frameworks":[],
@@ -18,7 +27,7 @@ export const projects = [
         "Description":""
     },
     {
-        "id":3,
+        "id":4,
         "Title":"",
         "Languages":[],
         "Frameworks":[],
