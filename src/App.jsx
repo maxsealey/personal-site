1087d0a37dc0980267e73733de0ca3ab2557efb7
Max Sealey <maxsealey@users.noreply.github.com>
2023-07-10 14:38:05 -0700

    Fixed redundant import label

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
 
 import Home from './pages/home/Home';
@@ -6,8 +7,6 @@ import About from './pages/about/About';
 import Projects from './pages/projects/Projects';
 import Experience from './pages/experience/Experience';
 import Contact from './pages/contact/Contact';
-
-import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
     return (
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './contact.scss';
 import Navbar from '../../components/navbar/Navbar';
-import Container from '../../components/content-template/ContentTemplate'
 import ContentTemplate from '../../components/content-template/ContentTemplate';
 
 const Contact = () => {
