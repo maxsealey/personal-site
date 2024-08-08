d173f64958001c6f57ea12bb5d1bae1465bf60cf
Max Sealey <maxsealey@users.noreply.github.com>
2024-08-07 21:57:55 -0700

    Fixed overflow issue with useEffect

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './about.scss';
 import Navbar from '../../components/navbar/Navbar';
 import ContentTemplate from '../../components/content-template/ContentTemplate';
+import AboutContent from '../../components/about-content/AboutContent';
 
 const About = () => {
+
+    useEffect(() => {
+        document.body.style.overflow = "auto";  
+        return () => {
+          document.body.style.overflow = "hidden";
+        }
+      }, [])
+
     return (
         <div className='about'>
             <Navbar/>
             <ContentTemplate heading="About Me" subtext=""/>
+            <AboutContent/>
         </div>
     )
 }
--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './experience.scss';
 import Navbar from '../../components/navbar/Navbar';
 import ContentTemplate from '../../components/content-template/ContentTemplate';
 
 const Experience = () => {
+    useEffect(() => {
+        document.body.style.overflow = "auto";  
+        return () => {
+          document.body.style.overflow = "hidden";
+        }
+      }, [])
     return (
         <div className="experience">
             <Navbar/>
