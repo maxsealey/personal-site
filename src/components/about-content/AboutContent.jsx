2c588a844def00ccd271d60cb7771f24931687cd
Max Sealey <maxsealey@users.noreply.github.com>
2024-08-07 21:59:30 -0700

    Created and styled AboutContent component

---
+++ src/components/about-content/AboutContent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './about-content.scss';
+
+const AboutContent = () => {
+    return (
+        <div className = "about-content-container">
+            <div className="about-content">
+                <h1>Personal Summary</h1>
+                <p>jhfifwsjcfoscpwscjhfif jhfifwsjcfoscpwscjhfif vjhfifwsjcfoscpwscjhfif jhfifwsjcfo scpwscjh fifjhfifwsjcfo scpwscjhfifjhf ifwsjcfoscp wscjhfifjhfifwsjcfoscpw scjhfif</p>
+                <h1>Work Philosophy</h1>
+                <p>dkjsfncsmclsmlsmvls</p>
+                <h1>Long-Term Goals</h1>
+                <p>euvidvoajovdiojagvori</p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutContent;
